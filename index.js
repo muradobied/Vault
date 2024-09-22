58201ec96d577a4f0de1b6800aa50a2c9d22581d
muradobied <morad.ebaid@optimumpartners.co>
2024-09-22 14:49:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-const secret = process.env.APP_SECRET;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Secret: ${secret}`);
+    res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
