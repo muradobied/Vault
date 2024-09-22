166a27f47c718b81f41578275771086c5a011b02
muradobied <morad.ebaid@optimumpartners.co>
2024-09-22 16:34:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running`);
 });
