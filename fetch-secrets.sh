49342a8ccf94c972e00c3e08ff41225bc980d06b
muradobied <morad.ebaid@optimumpartners.co>
2024-09-24 11:47:04 +0300

    Update fetch-secrets.sh

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Fetch the secret from Vault
-VAULT_ADDR='http://vaults.sociumtech.com:8200'
+VAULT_ADDR='http://vaults.sociumtech.com'
 VAULT_TOKEN='hvs.HijkKY1VZ7beBrehhCBCDxeP'
 SECRET=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" \
     $VAULT_ADDR/v1/secret/data/myapp | jq -r '.data.data.MY_SECRET')
