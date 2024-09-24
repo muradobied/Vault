34e90edf4a994172dfac775a7a00cf8c140dca08
muradobied <morad.ebaid@optimumpartners.co>
2024-09-24 12:31:43 +0300

    Update fetch-secrets.sh

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -2,11 +2,11 @@
 
 # Fetch the secret from Vault
 VAULT_ADDR='http://vaultstest.sociumtech.com'
-VAULT_TOKEN='your-vault-token'
+VAULT_TOKEN='hvs.MnPvLfGD2x5pumRxf4rSF9WG'
 SECRET_MESSAGE=$(curl \
-    --header "X-Vault-Token: root" \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
     --request GET \
-    http://127.0.0.1:8200/v1/secret/data/app | jq -r .data.data.SECRET_MESSAGE)
+    $VAULT_ADDR/v1/secret/data/app | jq -r .data.data.SECRET_MESSAGE)
 
 # Export the secret as an environment variable
 export SECRET_MESSAGE=$SECRET_MESSAGE
