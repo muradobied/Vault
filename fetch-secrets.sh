1de4d429cc32e7a535f95fc5e3537d660d83ce94
muradobied <morad.ebaid@optimumpartners.co>
2024-09-24 12:00:58 +0300

    Update fetch-secrets.sh

--- fetch-secrets.sh
+++ fetch-secrets.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Fetch Vault secret
+SECRET_KEY=$(vault kv get -field=MY_SECRET secret/myapp)
 
-# Fetch the secret from Vault
-VAULT_ADDR='http://vaults.sociumtech.com'
-VAULT_TOKEN='hvs.HijkKY1VZ7beBrehhCBCDxeP'
-SECRET=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" \
-    $VAULT_ADDR/v1/secret/data/myapp | jq -r '.data.data.MY_SECRET')
+# Check if the secret is fetched correctly
+if [ -z "$SECRET_KEY" ]; then
+  echo "Failed to fetch secret from Vault. Using default secret."
+  SECRET_KEY="default_secret"
+fi
 
-# Pass the secret to Docker Compose
-export MY_SECRET=$SECRET
+# Export secrets as environment variables
+export My_SECRET=$SECRET_KEY
+
 docker-compose up --build -d
